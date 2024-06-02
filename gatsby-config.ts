bd757118ac1308efe01ab7c7269b98bc2988b5f7
Shin'ya Ueoka <ueokande@i-beam.org>
2024-06-02 10:31:50 +0900

    use a new jsx transform

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,6 +1,7 @@
 import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
+  jsxRuntime: "automatic",
   siteMetadata: {
     title: "browser-actions-web",
     siteUrl: "https://www.yourdomain.tld",
